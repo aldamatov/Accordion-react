45e27f99651359bd87da2b2c9d5843ef75a0b0a0
Erzhan Aldamatov <33819603+aldamatov@users.noreply.github.com>
2022-06-27 23:53:03 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h2>questions and answers about login</h2>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
